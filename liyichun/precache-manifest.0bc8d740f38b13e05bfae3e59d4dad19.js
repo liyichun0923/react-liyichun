f5f52d46c8bdb6f0950e9540c359b673fccaced6
liyichun0923 <569275210@qq.com>
2019-10-27 18:07:15 +0800

    zuoye

---
+++ liyichun/precache-manifest.0bc8d740f38b13e05bfae3e59d4dad19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3321d5d4a9e695bd9c199d803db6335b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f271d96dd49f9d138f0d",
+    "url": "/static/css/main.4a37b1fa.chunk.css"
+  },
+  {
+    "revision": "bc737ead90919fdd7ce6",
+    "url": "/static/js/2.7a47c69e.chunk.js"
+  },
+  {
+    "revision": "f271d96dd49f9d138f0d",
+    "url": "/static/js/main.b361b888.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
